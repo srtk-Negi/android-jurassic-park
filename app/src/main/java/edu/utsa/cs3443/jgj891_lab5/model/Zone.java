9735d9556bc8cd2f63f1b059a5eb6b3cda35dd7a
Sarthak Negi <srnegi141@gmail.com>
2023-04-21 01:11:05 -0500

    changes made to Zone.java

---
+++ app/src/main/java/edu/utsa/cs3443/jgj891_lab5/model/Zone.java
@@ -0,0 +1,12 @@
+package edu.utsa.cs3443.jgj891_lab5.model;
+
+public class Zone {
+    private String zoneName;
+    private String zoneFullName;
+    private String riskLevel;
+    private int humanCount;
+
+    public Zone(){
+
+    }
+}
