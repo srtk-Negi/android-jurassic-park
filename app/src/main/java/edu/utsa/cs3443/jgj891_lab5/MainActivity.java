28a9274be2fb1d1b3ad95eaf09f723e272472d61
Sarthak Negi <Sarthak@Sarthaks-MacBook-Air.local>
2023-04-20 22:57:45 -0500

    activity_main.xml complete

--- app/src/main/java/edu/utsa/cs3443/jgj891_lab5/MainActivity.java
+++ app/src/main/java/edu/utsa/cs3443/jgj891_lab5/MainActivity.java
@@ -2,13 +2,37 @@ package edu.utsa.cs3443.jgj891_lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Intent moveToZoneActivityIntent = new Intent(MainActivity.this, ZoneActivity.class);
+
+        Button btn_x = findViewById(R.id.x_button);
+        Button btn_g = findViewById(R.id.g_button);
+        Button btn_d = findViewById(R.id.d_button);
+        Button btn_r = findViewById(R.id.r_button);
+        Button btn_b = findViewById(R.id.b_button);
+        Button btn_tr = findViewById(R.id.tr_button);
+        Button btn_ty = findViewById(R.id.ty_button);
+
+        btn_x.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(moveToZoneActivityIntent);
+            }
+        });
+
+
+
+
     }
 }
