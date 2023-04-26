36146fd73b79ab003cef75e7cffce0b05998a11b
Sarthak Negi <srnegi141@gmail.com>
2023-04-25 23:57:47 -0500

    Updated ZoneController.java

--- app/src/main/java/edu/utsa/cs3443/jgj891_lab5/controller/ZoneController.java
+++ app/src/main/java/edu/utsa/cs3443/jgj891_lab5/controller/ZoneController.java
@@ -6,16 +6,17 @@ import android.view.View;
 import edu.utsa.cs3443.jgj891_lab5.DinoActivity;
 import edu.utsa.cs3443.jgj891_lab5.MainActivity;
 import edu.utsa.cs3443.jgj891_lab5.ZoneActivity;
+import edu.utsa.cs3443.jgj891_lab5.model.Park;
 import edu.utsa.cs3443.jgj891_lab5.model.Zone;
 
 public class ZoneController implements View.OnClickListener {
-    private String zoneName;
-    private Zone zone;
+    private Park park;
     private ZoneActivity zoneActivity;
+    private MainActivity mainActivity;
+    private boolean read;
 
-    public ZoneController(ZoneActivity zoneActivity){
-        this.setZoneActivity(zoneActivity);
-//        this.setZoneName(zoneName);
+    public ZoneController(MainActivity mainActivity, boolean read){
+
     }
 
     /**
